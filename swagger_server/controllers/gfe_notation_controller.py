5afc21828b112e2e8a11180ddea2fcd781a77081
Muhammad haziq <m_haziq@outlook.com>
2019-10-31 16:34:00 +0500

    POST /gfe/{gene}/{locus} added

--- swagger_server/controllers/gfe_notation_controller.py
+++ swagger_server/controllers/gfe_notation_controller.py
@@ -3,21 +3,52 @@ from BioSQL import BioSeqDatabase
 from seqann.sequence_annotation import BioSeqAnn
 from pygfe.pygfe import pyGFE
 from seqann.gfe import GFE
+from Bio.SeqRecord import SeqRecord
+from Bio.Seq import Seq
+from pygfe.models.error import Error  # noqa: E501
+from pygfe.models.typing import Typing  # noqa: E501
+from pygfe.models.feature import Feature
+import io
+import logging
 
 
 def gfeNotation_post(sequence, locus, gene):
     kir = gene
-    sequence = sequence['sequence']
-    server = BioSeqDatabase.open_database(driver="pymysql", user="root",
-                                          passwd="my-secret-pw", host="0.0.0.0",
-                                          db="bioseqdb", port=3306)
+    sequence = SeqRecord(seq=Seq(sequence['sequence']))
+    log_capture_string = io.StringIO()
+    logger = logging.getLogger('')
+    logging.basicConfig(datefmt='%m/%d/%Y %I:%M:%S %p',
+                        level=logging.INFO)
+
+    # create console handler and set level to debug
+    ch = logging.StreamHandler(log_capture_string)
+    formatter = logging.Formatter(
+        '%(asctime)s - %(name)-35s - %(levelname)-5s - %(funcName)s %(lineno)d: - %(message)s')
+    ch.setFormatter(formatter)
+    ch.setLevel(logging.INFO)
+    logger.addHandler(ch)
+
     gfe = GFE()
     if kir:
-        seqann = BioSeqAnn(server=server, kir=kir)
+        seqann = BioSeqAnn(kir=True)
     else:
-        seqann = BioSeqAnn(server=server)
-    annotation = seqann.annotate(sequence)
-    features, gfe = gfe.get_gfe(annotation, locus)
+        seqann = BioSeqAnn()
+
+    try:
+        annotation = seqann.annotate(sequence)
+    except:
+        log_contents = log_capture_string.getvalue()
+        return Error("An error occured during the annotation", log=log_contents.split("\n")), 404
+    try:
+         res_feature ,res_gfe = gfe.get_gfe(annotation, locus)
+    except:
+        log_contents = log_capture_string.getvalue()
+        return Error("An error occured in getting the gfe of annotation", log=log_contents.split("\n")), 404
+    feats = []
+    for f in res_feature:
+        fn = Feature(accession=f.accession, rank=f.rank, term=f.term, sequence=f.sequence)
+        feats.append(fn)
     return {
-        'gfe': gfe
+        'gfe': res_gfe,
+        'feature': feats
     }
--- swagger_server/controllers/test_view_controller.py
+++
@@ -1,3 +0,0 @@
-
-def testView_get(sequence='sequence', locus='locus'):
-    return sequence + locus
