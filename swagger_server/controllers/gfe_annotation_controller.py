98fa19df970b25858f8dcd31e12e8edaa502768a
Muhammad haziq <m_haziq@outlook.com>
2019-10-31 16:36:20 +0500

    POST /gfe/seq/annotate/{gene}/{locus} added

--- swagger_server/controllers/gfe_annotation_controller.py
+++ swagger_server/controllers/gfe_annotation_controller.py
@@ -2,21 +2,79 @@ from Bio import SeqIO
 from BioSQL import BioSeqDatabase
 from seqann.sequence_annotation import BioSeqAnn
 from pygfe.pygfe import pyGFE
-from seqann.gfe import GFE
 
+import connexion
+import six
 
-def gfeAnnotation_post(sequence, locus, gene):
-    kir = gene
-    sequence = sequence['sequence']
-    server = BioSeqDatabase.open_database(driver="pymysql", user="root",
-                                          passwd="my-secret-pw", host="0.0.0.0",
-                                          db="bioseqdb", port=3306)
-    gfe = GFE()
-    if kir:
-        seqann = BioSeqAnn(server=server, kir=kir)
+from pygfe.models.error import Error  # noqa: E501
+from pygfe.models.typing import Typing  # noqa: E501
+from pygfe.models.feature import Feature
+
+from flask import request
+from swagger_server import util
+from seqann.sequence_annotation import BioSeqAnn
+from Bio.SeqRecord import SeqRecord
+from Bio.Seq import Seq
+import logging
+import io
+import re
+
+seqanns = {}
+
+def gfeAnnotation_post(sequence, gene, locus=None, imgthla_version="3.31.0"):
+    global seqanns
+
+    typing = Typing()
+    sequence = SeqRecord(seq=Seq(sequence['sequence']))
+
+    if not re.match(".", imgthla_version):
+        imgthla_version = ".".join([list(imgthla_version)[0],
+                                    "".join(list(imgthla_version)[1:3]),
+                                    list(imgthla_version)[3]])
+
+    db = "".join(imgthla_version.split("."))
+    log_capture_string = io.StringIO()
+    logger = logging.getLogger('')
+    logging.basicConfig(datefmt='%m/%d/%Y %I:%M:%S %p',
+                        level=logging.INFO)
+
+    # create console handler and set level to debug
+    ch = logging.StreamHandler(log_capture_string)
+    formatter = logging.Formatter(
+        '%(asctime)s - %(name)-35s - %(levelname)-5s - %(funcName)s %(lineno)d: - %(message)s')
+    ch.setFormatter(formatter)
+    ch.setLevel(logging.INFO)
+    logger.addHandler(ch)
+
+    if db in seqanns:
+        seqann = seqanns[db]
+    elif gene:
+        seqann = BioSeqAnn(verbose=True, safemode=True, dbversion=db, verbosity=3, kir=True)
+        seqanns.update({db: seqann})
     else:
-        seqann = BioSeqAnn(server=server)
-    annotation = seqann.annotate(sequence, locus)
-    return {
-        'annotation': annotation
-    }
+        seqann = BioSeqAnn(verbose=True, safemode=True, dbversion=db, verbosity=3)
+        seqanns.update({db: seqann})
+
+    try:
+        annotation = seqann.annotate(sequence, locus)
+    except:
+        log_contents = log_capture_string.getvalue()
+        return Error("An error occured during the annotation", log=log_contents.split("\n")), 404
+
+    if not annotation:
+        log_contents = log_capture_string.getvalue()
+        return Error("No annotation could be produced", log=log_contents.split("\n")), 404
+
+    if not hasattr(annotation, 'structure'):
+        log_contents = log_capture_string.getvalue()
+        return Error("No structure was produced", log=log_contents.split("\n")), 404
+
+    feats = []
+    for f in annotation.structure:
+        fn = Feature(accession=f.accession, rank=f.rank, term=f.term, sequence=f.sequence)
+        feats.append(fn)
+
+    typing.features = feats
+    typing.gfe = annotation.gfe
+    typing.imgtdb_version = imgthla_version
+    return typing
